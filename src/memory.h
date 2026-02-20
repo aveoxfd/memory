241f09802682389c465ee672e81e85fd81cdfe64
.ffd <krekerbruh@hotmail.com>
2026-02-20 20:55:56 +0300

    macro update

---
+++ src/memory.h
@@ -0,0 +1,24 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#if defined(MEMORY_STATIC)
+    #define MEM_API
+    #elif defined(_WIN32) || defined(_WIN64) || defined(_CYGWIN__)
+        #ifdef MEM_BUILD_DLL
+            #define MEM_API __declspec(dllexport)
+        #else
+            #define MEM_API __declspec(dllimport)
+    #endif
+    #else
+        #define MEM_API
+#endif
+
+#define FREE        0b00000001
+#define IS_USING    0b00000010
+
+typedef unsigned long long size_t;
+
+MEM_API void* cmalloc(size_t size);
+MEM_API void cfree(void *block);
+
+#endif
