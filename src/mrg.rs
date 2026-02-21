a73d79ac753a8eb76bd9470434b5b345a4af8c6e
.ffd <krekerbruh@hotmail.com>
2026-02-21 14:32:17 +0300

    merge_block v1.1.1

--- src/mrg.rs
+++ src/mrg.rs
@@ -1,4 +1,9 @@
 //merge blocks
+/*
+the block will be merged if:
+-it has odd number of size;
+-it less then  
+*/
 
 #[repr(C)] //tells the compiler to use c-style struct layout
 struct BlockHeader {
@@ -9,9 +14,12 @@ struct BlockHeader {
 
 fn merge_blocks(list : *mut BlockHeader) {
     unsafe{
-        let mut current : *mut BlockHeader = list;
+        let current : *mut BlockHeader = list;
         while !current.next{
-            
+
+            if current.size % 2 != 0
+
+            current =  current.next;
         }
     }
 }
