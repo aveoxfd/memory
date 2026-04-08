e3b2071e33bc95df9e5181dc857f7a21d42fb92c
.ffd <krekerbruh@hotmail.com>
2026-04-08 10:12:35 +0300

    memory.h v1.2

--- src/memory.h
+++ src/memory.h
@@ -9,6 +9,12 @@
 #endif
 #endif
 
+#if defined (USEDLL)
+#if defined (_WIN32) || defined (_WIN64)
+#define MEM_API __declspec(dllexport)
+#endif
+#endif
+
 #define IS_FREE         0b00000001
 #define IS_USING        0b00000010
 
@@ -17,5 +23,6 @@ typedef unsigned long long size_t;
 MEM_API void* cmalloc(size_t size);
 MEM_API void cfree(void *block); //free with merge
 MEM_API void* crealloc(void *block, size_t size);
+MEM_API void memcpy_av(void *restrict dst, void *restrict src, const unsigned long size);
 
 #endif
