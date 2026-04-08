e408486e875717c702ffec52dbd247184a1c9a9c
.ffd <krekerbruh@hotmail.com>
2026-04-08 10:53:09 +0300

    genlib v0.1.2

--- genlib.bat
+++ genlib.bat
@@ -1,6 +1,6 @@
 gcc -shared -o lib/memory.dll src/memory.c -D DLLBLD
 
 gcc -c src/memory.c -o obj/memory.o
-ar rst lib/memory.lib obj/memory.o
+ar rc lib/memory.lib obj/memory.o
 
 nm lib/memory.lib
